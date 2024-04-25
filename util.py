fixing R1 gradient issue

util.py
@@ -114,10 +114,10 @@ def count_parameters(model, encoder):
 def compute_gradient_penalty(discriminator, input, real_data, generated_data):
     batch_size = real_data.size(0)
     device = real_data.device
-    
+
     # Generate random epsilon
     epsilon = torch.rand(batch_size, 1, 1, 1).to(device)
-    
+
     # Create interpolated data
     interpolated_data = epsilon * real_data + (1 - epsilon) * generated_data
     interpolated_data.requires_grad_(True)  # Ensure requires_grad is set to True
@@ -125,10 +125,10 @@ def compute_gradient_penalty(discriminator, input, real_data, generated_data):
     # Compute discriminator scores for interpolated data
     mixed_scores = discriminator(input, interpolated_data)
 
-    # Compute gradients of the scores with respect to the interpolated data
+    # Compute gradient penalty
     gradients = torch.autograd.grad(outputs=mixed_scores, inputs=interpolated_data,
                                     grad_outputs=torch.ones_like(mixed_scores),
-                                    create_graph=True, retain_graph=True)[0]
+                                    create_graph=True)[0]
 
     # Compute gradient penalty
     gradient_penalty = (gradients.view(batch_size, -1).norm(2, dim=1) ** 2).mean()